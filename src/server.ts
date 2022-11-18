fc42b77cc34354df428ee1269431242c7f31e5c2
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-18 14:27:54 -0300

    implementado rota de post para categories com classes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  const { name } = request.body;
-  return response.json({ message: `Hello ${name}` });
-})
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
